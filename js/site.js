8e74801842e4aa989e4cf031bf0922ff0c8b3b1f
tj-longshore <76498230+tj-longshore@users.noreply.github.com>
2021-12-22 16:43:57 -0500

    Completed .reverse App
    Completed .reverse App

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//Get the user entered string from the page
+//controller function
+function getString(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let originalString = document.getElementById("originalString").value;
+
+    let revString = reverseString(originalString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//logic function
+function reverseString(originalString){
+
+    let revString = [];
+
+    //reverse string with a for loop
+    let userString = document.getElementById("originalString").value;
+    for (let i = userString.length - 1; i >= 0; i--){
+        revString += userString[i];
+    }
+    return revString;
+}
+
+//Display the message
+//view function
+function displayString(revString){
+    
+    //Display message to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
